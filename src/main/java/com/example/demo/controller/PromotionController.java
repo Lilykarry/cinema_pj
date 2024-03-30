795ff761663227123f43239c040234c4feadee6b
quantrinh123 <quantrinh29092003@gmail.com>
2024-03-30 18:41:55 +0700

    showtimeAndPromotion

---
+++ src/main/java/com/example/demo/controller/PromotionController.java
@@ -0,0 +1,28 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Endow;
+import com.example.demo.model.Threat;
+import com.example.demo.service.PromotionService;
+import com.example.demo.service.ShowTimeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/promotion")
+public class PromotionController {
+    @Autowired
+    private PromotionService promotionService;
+    @GetMapping
+    public String showTime(Model model){
+        List<Endow> endows = promotionService.listAll();
+
+        model.addAttribute("endows", endows);
+
+        return "home/promotion";
+    }
+}
